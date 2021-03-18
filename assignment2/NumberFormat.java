da3084b6aaaf4f475c6ba5630a6ba1ee34dafd0a
Tarun999-King <79780623+Tarun999-King@users.noreply.github.com>
2021-03-18 16:26:11 +0530

    Add files via upload

---
+++ assignment2/NumberFormat.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+import java.util.*;
+
+public class NumberFormat {
+	
+	public static void main( String[] arg)
+		{
+	int age;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please Enter your age : ");
+	//throws Exception as if the input string is of illegal format for parsing as it as null or alphanumeric.
+	age = Integer.parseInt(sc.next());
+	System.out.println("Your age is : " +age);
+	}
+	}
---
+++ assignment2/StringToChar.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+public class StringToChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        String str = "SANDEEP KUMAR";
+        char[] ch = new char[str.length()]; 
+        for (int i = 0; i < str.length(); i++) 
+        { 
+            ch[i] = str.charAt(i); 
+        }
+        for (char c : ch)
+        { 
+            System.out.println(c); 
+
+        }
+	}
+
+}
+
